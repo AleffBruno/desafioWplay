3a12c9ff7f28745a3628bbad90330b13b33bdd2a
Aleff <aleffbruno@gmail.com>
2018-09-18 08:17:44 -0300

    next step, add owner file/path

--- junk/failed.ts
+++ junk/failed.ts
@@ -2,11 +2,10 @@
 
 import * as fs from 'fs';
 import * as Path from 'path';
-const path = "C:/Users/ffela/Desktop/myPath"
+const path = "C:/Users/f1608101/Desktop/myPath"
 
 import {Folder} from '../src/models/Folder';
 import {File} from '../src/models/File';
-import { S_IWUSR } from 'constants';
 
 function returnPathName(){
     return "a";
@@ -32,6 +31,11 @@ function returnPathItems(path:any){
         if(!fs.lstatSync(filePath).isDirectory()){
             let file = new File();
             file.name = Path.basename(filePath);
+            file.path = filePath;
+            file.extension = Path.extname(filePath);
+            file.creationDate = fs.statSync(filePath).birthtime.toString();
+            file.size = fs.statSync(filePath).size.toString();
+            
 
             fileItems.push(file);
             //fileItems.push(filePath);
@@ -54,16 +58,9 @@ function returnPathItems(path:any){
     return myFolder;
 }
 
-let arr : any = {
-    name:returnPathName(),
-    //items:returnPathItems(path)
-};
-
 //returnPathItems(path);
 console.log(JSON.stringify(returnPathItems(path)));
 //arr = JSON.stringify(arr); // {"name":"a","items":["a","b"]}
-//console.log(arr);
-//console.log({arr}); // { arr: '["a","b"]' }
 //console.log(fs.statSync(path).S_IWUSR);
-//console.log(Path.basename(path));
+//console.log(fs.statSync(path).uid);
 
